51c4c606831bbe61f496df95f87ad43ae95e99a6
nubisub <hendraksm18@mail.ugm.ac.id>
2022-07-15 22:24:50 +0700

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import {Box, Container, Grid, Paper, Slider, Stack, styled, Switch} from "@mui/material";
+import {useState} from "react";
+import {VolumeDown, VolumeUp} from "@mui/icons-material";
+
+
+const Option = (props) => {
+    return(
+        <div  className="grid grid-cols-1 gap-4 max-w-xl ">
+            <div>
+                <p>Power</p>
+
+                <Switch
+                    // checked={checked}
+                    // onChange={handleChange}
+                    // inputProps={{ 'aria-label': 'controlled' }}
+                />
+            </div>
+            <div>{props.name} </div>
+            <div>
+                <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+                    <VolumeDown />
+                    <Slider aria-label="Volume"
+                            // value={value}
+                            // onChange={handleChange}
+                    />
+                    <VolumeUp />
+                </Stack>
+            </div>
+            <div>
+                <Switch
+                    // checked={checked}
+                    // onChange={handleChange}
+                    // inputProps={{ 'aria-label': 'controlled' }}
+                />
+                <p>Bank</p>
+            </div>
+        </div>
+    )
+}
+
+
+const Button = () => {
+
+    const[name, setName] = useState(" ")
+
+    return(
+<>
+            <div className="grid grid-cols-3 gap-4 max-w-xl " >
+                <div onClick={() => setName("Heater")} className="bg-fuchsia-500" >1</div>
+                <div onClick={() => setName("Heater 2")} className="bg-fuchsia-500">2</div>
+                <div onClick={() => setName("Heater 3")} className="bg-fuchsia-500">3</div>
+                <div onClick={() => setName("Heater 4")} className="bg-fuchsia-500">4</div>
+                <div onClick={() => setName("Clap")} className="bg-fuchsia-500">5</div>
+                <div onClick={() => setName("Open HH")} className="bg-fuchsia-500">6</div>
+                <div onClick={() => setName("Kick n'Hat")} className="bg-fuchsia-500">7</div>
+                <div onClick={() => setName("Kick")} className="bg-fuchsia-500">8</div>
+                <div onClick={() => setName("Closed HH")} className="bg-fuchsia-500">9</div>
+            </div>
+                <Option name={name} />
+</>
+
+
+)
+
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App h-screen flex justify-center items-center ">
+          <div className="grid grid-cols-2 gap-4 max-w-xl w-96 " >
+              <Button/>
+          </div>
+      </div>
+
   );
 }
 
